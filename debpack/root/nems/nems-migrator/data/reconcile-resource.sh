ed7e3b3abc9c0a389c2968c24b7d7e4c15c2ab3f
NEMS Linux <nems@category5.tv>
2024-03-23 11:05:34 -0400

    Fix resource reconciliation

--- debpack/root/nems/nems-migrator/data/reconcile-resource.sh
+++ debpack/root/nems/nems-migrator/data/reconcile-resource.sh
@@ -5,13 +5,13 @@
 # Traditional "replacement" of the config file would result in those new settings missing.
 # So this script consolidates the data between the source (backup.nems) and destination (resource.cfg).
 
-echo PHP_EOL . "Reconciling Nagios resource.cfg against backup:" . PHP_EOL;
-
 $resourcesrc = $argv[1];
 $resourcedest = $argv[2];
 
 $source = '/tmp/nems_migrator_restore' . $resourcesrc . '/resource.cfg';
 $dest   = $resourcedest . '/resource.cfg';
+
+echo PHP_EOL . "Reconciling Nagios resource.cfg against backup:" . PHP_EOL;
 
 // Load source file contents
 $sourceContent = file_get_contents($source);
@@ -27,24 +27,6 @@ if ($destContent === false) {
     exit(1);
 }
 
-// Define the new header
-$newHeader = <<<HEADER
-###########################################################################
-#
-# RESOURCE.CFG - Resource File for Nagios
-#
-# This file is configured using the NEMS System Settings Tool
-# Please do not edit it directly.
-#
-###########################################################################
-HEADER;
-
-// Check if the new header is already present in the destination file
-if (strpos($destContent, $newHeader) === false) {
-    // Replace the header in the destination file
-    $destContent = preg_replace('/^#{2,}.*?(?=^#{2,}|\z)/ms', $newHeader . PHP_EOL, $destContent, 1);
-}
-
 // Consolidate settings from source to destination
 foreach (explode(PHP_EOL, $sourceContent) as $line) {
     $line = trim($line);
@@ -53,6 +35,11 @@ foreach (explode(PHP_EOL, $sourceContent) as $line) {
         $variable = trim(substr($parts[0], 1));
         echo "Importing: \033[97m" . "$" . $variable . "\033[0m" . PHP_EOL;
         $value = isset($parts[1]) ? trim($parts[1]) : '';
+
+        // Skip USER1 and USER2, as these are system settings and should not be replaced
+        if ($variable == 'USER1$' || $variable == 'USER2$') {
+           continue;
+        }
 
         // Check if the variable already exists in destination content
         $existingLine = '$' . $variable . '=';
